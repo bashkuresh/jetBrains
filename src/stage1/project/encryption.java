06ee2e0f4a44da401bdc9c7b2d8d5a8628c30fd4
arthursmith <bashkuresh1992@gmail.com>
2020-04-18 16:10:39 -0400

    first stage

---
+++ src/stage1/project/encryption.java
@@ -0,0 +1,19 @@
+
+package stage1.project;
+
+public class encryption {
+    public static void main(String[] args) {
+
+        String message = "we found a treasure!";
+        StringBuilder cipherText = new StringBuilder();
+
+        for (char c : message.toCharArray()) {
+            if (Character.isAlphabetic(c)) {
+                c = (char) (123 - (c - 96));
+            }
+            cipherText.append(c);
+        }
+
+        System.out.println(cipherText);
+    }
+}
